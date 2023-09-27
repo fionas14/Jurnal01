48b75538a861f4f641a346b28bdd3f2e3eaa770f
fionas14 <siregarfiona90@gmail.com>
2023-09-27 13:31:41 +0700

    first commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello{
+    public static void main (String[] args){
+System.out.println("Nama : Fiona Siregar");
+System.out.println("NIM : 607062330032");
+System.out.println("Kelas : D3IF-47-03");
+
+
+    }
+
+}
